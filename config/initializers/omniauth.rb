dd9ffc451589b9b0460e3b3ef0abddfb792db9d4
Luis Bajana <lbajana@gmail.com>
2013-10-21 16:40:47 -0500

    First commit, devise and omniauth gems working together

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :twitter, 'Hi3rCbdICjfmPssX0CzCg', '0lI17ZeGYPvk2vQLARd0aPsFuaraEOYBOXE9b0gIs'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterAuthentication::Application.config.secret_token = '64f811dc9b3c796ad46ec2f6f235c1c2f042471bc0abb5c4e2aa2e235ba34684e90175b36c1cf340e7f86c89b65abf4db313442b19a36cf5cd2e20040a0c23bf'
